6f69d5079bfc93b860860afe6e5963a7b6d63349
clarjon1 <info@clarjon1.com>
2023-11-14 23:28:11 -0500

    v1.8

--- README.md
+++ README.md
@@ -20,11 +20,12 @@ however there are a few more modes available, documented below.
  -duration <int> -- Sets the duration of the zap or buzz! Defaults to 1 second
  -buzzduration <int> -- Sets the duration of the buzz! Defaults to -duration's set value
  -op [1-3] -- Not used yet! Will be added for CustomZap mode later
- -mode [1-4] -- Sets the mode this will run in. Defaults to 1 Current values:
-   -mode 1 -- default zap -- default buzz, wait (5s) and shock
-   -mode 2 -- random zap -- buzz, wait for $randmin up to $randmax, then shock
-   -mode 3 -- fakeout -- will only buzz
-   -mode 4 -- no warning -- zap only, no warning
+ -mode [1-5] -- Sets the mode this will run in. Defaults to 1 Current values:
+   -mode 1 -- DefaultZap -- default buzz, wait (5s) and shock
+   -mode 2 -- RandomZap -- buzz, wait for $randmin up to $randmax, then shock
+   -mode 3 -- Fakeout -- will only buzz
+   -mode 4 -- NoWarning -- zap only, no warning
+   -mode 5 -- Ramp Mode - Will ramp up to the set amount. Will zap at 10%, 25%, 50%, 75% and then 100% of set intensity, with a delay between each zap
  -randmin <int> -- Minimum number of seconds for RandoZap mode delay range
  -randmax <int> -- maximum number of seconds for RandoZap Mode delay range
  -delay <int> -- Sets the delay between warning buzz and zap. Defaults to 5 seconds
--- pishock.ps1
+++ pishock.ps1
@@ -5,11 +5,12 @@
 ## OPTIONAL:
 # -duration <int> -- Sets the duration of the zap or buzz!
 # -op [1-3] -- Not used yet! Will be added for CustomZap mode later
-# -mode [1-4] -- Sets the mode this will run in. Defaults to 1 Current values:
+# -mode [1-5] -- Sets the mode this will run in. Defaults to 1 Current values:
   # -mode 1 -- DefaultZap -- default buzz, wait (5s) and shock
   # -mode 2 -- RandomZap -- buzz, wait for $randmin up to $randmax, then shock
   # -mode 3 -- Fakeout -- will only buzz
   # -mode 4 -- NoWarning -- zap only, no warning
+  # -mode 5 -- Ramp Mode - Will ramp up to the set amount. Will zap at 10%, 25%, 50%, 75% and then 100% of set intensity, with a delay between each zap
 # -randmin <int> -- Minimum number of seconds for RandomZap mode delay range
 # -randmax <int> -- maximum number of seconds for RandomZap Mode delay range
 # -delay <int> -- Sets the delay between warning buzz and zap. Defaults to 5 seconds
@@ -108,6 +109,30 @@ $funcDef = ${function:CallAPI}.ToString()
   }
 }
 
+function RampingZap {
+  $funcDef = ${function:CallAPI}.ToString()
+   $shockCodeArray | foreach-Object -parallel {
+    [int]$ramp1 = $using:intensity * (10/100)
+    [int]$ramp2 = $using:intensity * (25/100)
+    [int]$ramp3 = $using:intensity * (50/100)
+    [int]$ramp4 = $using:intensity * (75/100)
+
+     Write-output "Working on $_"
+     ${function:CallAPI} = $using:funcDef
+     CallAPI -shockerCode $_ -op 1 -apikey $using:pishockAPIKey  -intensity $using:intensity -duration $using:buzzduration -pishockUsername $using:pishockUsername
+     $delay = $using:delay + $using:buzzduration
+     Start-Sleep -Seconds $delay
+     CallAPI -shockerCode $_ -op 0 -apikey $using:pishockAPIKey -intensity $ramp1 -duration $using:duration  -pishockUsername $using:pishockUsername
+     Start-Sleep -Seconds $delay
+     CallAPI -shockerCode $_ -op 0 -apikey $using:pishockAPIKey -intensity $ramp2 -duration $using:duration  -pishockUsername $using:pishockUsername
+     Start-Sleep -Seconds $delay
+     CallAPI -shockerCode $_ -op 0 -apikey $using:pishockAPIKey -intensity $ramp3 -duration $using:duration  -pishockUsername $using:pishockUsername
+     Start-Sleep -Seconds $delay
+     CallAPI -shockerCode $_ -op 0 -apikey $using:pishockAPIKey -intensity $ramp4 -duration $using:duration  -pishockUsername $using:pishockUsername
+     Start-Sleep -Seconds $delay
+     CallAPI -shockerCode $_ -op 0 -apikey $using:pishockAPIKey -intensity $using:intensity -duration $using:duration  -pishockUsername $using:pishockUsername
+    }
+  }
 function RandomZap {
 $funcDef = ${function:CallAPI}.ToString()
  $shockCodeArray | foreach-Object -parallel {
@@ -145,4 +170,5 @@ switch ($mode){
     2 {RandomZap}
     3 {Fakeout}
     4 {NoWarning}
+    5 {RampingZap}
 }
